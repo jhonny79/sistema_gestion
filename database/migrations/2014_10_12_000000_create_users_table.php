483a2bab27d3bcb86407cdfc93f145b6cf1fe9b5
Recosur <47953666+Recosur@users.noreply.github.com>
2019-06-03 15:13:10 -0400

    Add files via upload

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('last-name');
+            $table->string('cargo');
+            $table->string('p00');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
