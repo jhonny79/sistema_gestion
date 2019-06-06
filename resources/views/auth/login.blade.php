17e60bb8c8950fcf3391941949a1fd06df0e2b22
jhonny hernandez <jhha1979@gmail.com>
2019-06-06 12:56:57 -0400

    actualizado por jhonny

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="container">
-    <div class="muestra">
+    {{-- <div class="muestra"> --}}
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
